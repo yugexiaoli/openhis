78837be74e6711344d745d10a454f743ff352fb0
陈昶余 <5567444+chen_chang_yu@user.noreply.gitee.com>
2020-10-31 07:09:40 +0800

    删除a目录下的2.txt文件

--- open-his/his-system/system-mapper/src/main/java/com/twofish/mapper/MenuMapper.java
+++
@@ -1,7 +0,0 @@
-package com.twofish.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.twofish.domain.Menu;
-
-public interface MenuMapper extends BaseMapper<Menu> {
-}
--- open-his/his-system/system-mapper/src/main/java/com/twofish/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.twofish.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.twofish.domain.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
