3a34c71daa93f653ef095560bd40b9c5bb968561
陈昶余 <5567444+chen_chang_yu@user.noreply.gitee.com>
2020-11-02 14:51:59 +0800

    删除文件

--- open-his/his-system/system-mapper/src/main/java/com/twofish/mapper/DictDataMapper.java
+++
@@ -1,7 +0,0 @@
-package com.twofish.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.twofish.domain.DictData;
-
-public interface DictDataMapper extends BaseMapper<DictData> {
-}
--- open-his/his-system/system-mapper/src/main/java/com/twofish/mapper/DictTypeMapper.java
+++
@@ -1,7 +0,0 @@
-package com.twofish.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.twofish.domain.DictType;
-
-public interface DictTypeMapper extends BaseMapper<DictType> {
-}
